0a126ed68981dba97c246c243e8102b67fbb7cba
olmaneuh <olmaneuh@gmail.com>
2021-03-07 21:23:42 -0600

    Scripts: Fix spacing and break lines in script

--- scripts/deploy_orders_microservice.sh
+++ scripts/deploy_orders_microservice.sh
@@ -3,7 +3,7 @@
 printf "Building and deploying orders service...\n"
 
 # Setup env varibles
-printf "Setting up env variables..."
+printf "Setting up env variables...\n"
 export REGION="us-east1"
 export GOOGLE_CLOUD_PROJECT=$(gcloud config get-value project)
 export SERVICE_SRC="../services/orders"
@@ -11,35 +11,35 @@ export SERVICE_NAME="orders"
 printf "Completed.\n"
 
 # Enable APIs
-printf "Enabling Cloud Build API..."
+printf "Enabling Cloud Build API...\n"
 gcloud services enable cloudbuild.googleapis.com
 printf "Completed.\n"
-printf "Enabling Cloud Run API..."
+printf "Enabling Cloud Run API...\n"
 gcloud services enable run.googleapis.com
 printf "Completed.\n"
 
 # Setup default project values
-printf "Setting up default project values..."
+printf "Setting up default project values...\n"
 gcloud config set run/region ${REGION}
 gcloud config set run/platform managed
 printf "Completed.\n"
 
 # Build microservice
-printf "Building orders service..."
+printf "Building orders service...\n"
 gcloud builds submits \
     ${SERVICE_SRC} \
     --tag gcr.io/${GOOGLE_CLOUD_PROJECT}/${SERVICE_NAME}
 printf "Completed.\n"
 
 # Deploy microservice
-printf "Deploying orders service..."
+printf "Deploying orders service...\n"
 gcloud run deploy ${SERVICE_NAME} \
     --image gcr.io/${GOOGLE_CLOUD_PROJECT}/${SERVICE_NAME}
     --no-allow-unauthenticated
 printf "Completed.\n"
 
 # Unset Env Varibles
-printf "Unsetting env variables..."
+printf "Unsetting env variables...\n"
 unset REGION
 unset GOOGLE_CLOUD_PROJECT
 unset SERVICE_SRC
